ac21642726bf48c46a2493f57a5a540b6251eb6f
galperie <11galpel@gmail.com>
2017-06-05 17:09:32 -0400

    Push arrow to lower part of the page

--- main.js
+++ main.js
@@ -18,6 +18,10 @@ clipboard.on('error', function(e) {
 $(function() {
 	$('a[href*=#]').on('click', function(e) {
 		e.preventDefault();
-		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+		$('html, body').animate({
+       scrollTop: $(
+         $(this).attr('href')
+       ).offset().top
+     }, 500, 'linear');
 	});
 });
